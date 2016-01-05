8ccf808984621efaf3068c03a4e4b48f0dea39d1
Alex Rutherford <alexisadams@gmail.com>
2016-01-05 10:10:20 -0500

    fixed secrets

--- secrets.py
+++
@@ -1,5 +0,0 @@
-# Refresh token with `curl 'https://graph.facebook.com/oauth/access_token?client_id=1607578872796964&client_secret=77f7a51650ae3e9137616ecde3ef0c1a&grant_type=client_credentials'`
-
-# ! curl 'https://graph.facebook.com/oauth/access_token?client_id=1607578872796964&client_secret=77f7a51650ae3e9137616ecde3ef0c1a&grant_type=client_credentials'
-
-ACCESSTOKEN='1607578872796964|HufuwBfEHNNjcunAP3YrH50uoUc'
